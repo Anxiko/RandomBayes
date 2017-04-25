8f7022e8524f0b5acfae0c55fa230659b5572f40
Joaquín Monteagudo Gómez @ Tower <Kindos7@gmail.com>
2017-04-26 00:14:14 +0200

    Started implementing buildClassifier

--- weka/classifiers/bayes/RandomBayes.java
+++ weka/classifiers/bayes/RandomBayes.java
@@ -3,6 +3,8 @@ package weka.classifiers.bayes;
 import java.util.Collections;
 import java.util.Enumeration;
 import java.util.Vector;
+import java.util.ArrayList;
+import java.util.List;
 
 import weka.classifiers.AbstractClassifier;
 import weka.core.Aggregateable;
@@ -24,11 +26,73 @@ import weka.estimators.DiscreteEstimator;
 import weka.estimators.Estimator;
 import weka.estimators.KernelEstimator;
 import weka.estimators.NormalEstimator;
+import weka.classifiers.bayes.NaiveBayes;
 
 /**
  *
  * @author Kindo
  */
-public class RandomBayes {
+public class RandomBayes extends AbstractClassifier{
+    
+    /* Config */
+    
+    /*Default options*/
+    
+    //Number of classifiers
+    public static final int DEF_N_CLASSIFIERS = 10;
+    
+    //Percentage of instances used in each classifier
+    public static final float DEF_PERC_INSTANCES = 0.6f;
+    
+    //Percentage of features used in each classifier
+    public static final float DEF_PERC_FEAT = 0.6f;
+    
+    /* Data */
+    
+    /*Parameters*/
+    
+    //Number of classifiers
+    int n_classifiers;
+    
+    //Percentage of instances
+    float perc_instances;
+    
+    //Percentages of features
+    float perc_feat;
+    
+    /*Bagging*/
+    
+    //Bag of classifiers
+    NaiveBayes[] bag;
+    
+    
+    /* Methods */
+    
+    /*Constructors*/
+    
+    public RandomBayes(){
+        //Call parent constructor
+        super();
+        
+        //Set default parameters
+        n_classifiers = DEF_N_CLASSIFIERS;
+        perc_instances = DEF_PERC_INSTANCES;
+        perc_feat = DEF_PERC_FEAT;
+        
+        //Bag of classifiers
+        bag = new NaiveBayes[n_classifiers];
+    }
+    
+    /*Classifier*/
+    
+    //Train the classifier with the given instances
+    @Override
+    public void buildClassifier(Instances data){
+        //Train all the NaiveBayes
+        for (int i  = 0;i<n_classifiers;++i){
+            bag[i] = new weka.classifiers.bayes.NaiveBayes();
+            //weka.filters.unsupervised.instance.Resample;
+        }
+    }
     
 }
