7f1c0c0c3034c163ee4addd9c190ff79294e296f
Joaquín Monteagudo Gómez @ Tower <Kindos7@gmail.com>
2017-04-25 21:50:54 +0200

    Moved file location

---
+++ weka/classifiers/bayes/RandomBayes.java
@@ -0,0 +1,34 @@
+package weka.classifiers.bayes;
+
+import java.util.Collections;
+import java.util.Enumeration;
+import java.util.Vector;
+
+import weka.classifiers.AbstractClassifier;
+import weka.core.Aggregateable;
+import weka.core.Attribute;
+import weka.core.Capabilities;
+import weka.core.Capabilities.Capability;
+import weka.core.Instance;
+import weka.core.Instances;
+import weka.core.Option;
+import weka.core.OptionHandler;
+import weka.core.RevisionUtils;
+import weka.core.TechnicalInformation;
+import weka.core.TechnicalInformation.Field;
+import weka.core.TechnicalInformation.Type;
+import weka.core.TechnicalInformationHandler;
+import weka.core.Utils;
+import weka.core.WeightedInstancesHandler;
+import weka.estimators.DiscreteEstimator;
+import weka.estimators.Estimator;
+import weka.estimators.KernelEstimator;
+import weka.estimators.NormalEstimator;
+
+/**
+ *
+ * @author Kindo
+ */
+public class RandomBayes {
+    
+}
