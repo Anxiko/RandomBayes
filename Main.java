a31bd3f9e303f90a218c25d6d9b90c1e6208abbd
Joaquín Monteagudo Gómez @ Tower <Kindos7@gmail.com>
2017-04-26 17:45:37 +0200

    Testing the algorithm in Main

--- Main.java
+++ Main.java
@@ -1,6 +1,9 @@
 
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import weka.core.Instances;
+import weka.core.converters.ConverterUtils.DataSource;
+import weka.classifiers.bayes.RandomBayes;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -18,15 +21,22 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
-        String s = "CLASSIFIER weka.classifiers.trees.J48 -U FILTER weka.filters.unsupervised.instance.Randomize DATASET iris.arff";
-        
-        String[] weka_args= s.split(" ");
-        
         try {
-            //Call the demo
-            WekaDemo.main(weka_args);
+            //Name of the data file
+            String filename = "iris.arff";
+            DataSource source = new DataSource(filename);
+            Instances data = source.getDataSet();
+            // setting class attribute if the data format does not provide this information
+            // For example, the XRFF format saves the class attribute information as well
+            if (data.classIndex() == -1)
+                data.setClassIndex(data.numAttributes() - 1);
+            
+            RandomBayes classifier = new RandomBayes();//Create the classifier (default config)
+            
+            classifier.buildClassifier(data);//Build it using the read dataset
+            
         } catch (Exception ex) {
+            ex.printStackTrace();
         }
     }
     
