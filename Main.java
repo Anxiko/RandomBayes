350ec8697d753ff89fb7b131aac6f048fc8123ab
Joaquín Monteagudo Gómez @ Tower <Kindos7@gmail.com>
2017-06-13 13:38:40 +0200

    Modified Main file to use the Weka API

--- Main.java
+++ Main.java
@@ -1,6 +1,15 @@
 
+import java.util.Arrays;
+import java.util.Enumeration;
+import java.util.Random;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import weka.classifiers.Evaluation;
+import weka.core.Instances;
+import weka.core.converters.ConverterUtils.DataSource;
+import weka.classifiers.bayes.RandomBayes;
+import weka.core.Attribute;
+import weka.core.Instance;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -18,15 +27,24 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
-        String s = "CLASSIFIER weka.classifiers.trees.J48 -U FILTER weka.filters.unsupervised.instance.Randomize DATASET iris.arff";
-        
-        String[] weka_args= s.split(" ");
-        
         try {
-            //Call the demo
-            WekaDemo.main(weka_args);
+            //Name of the data file
+            String filename = "iris.arff";
+            DataSource source = new DataSource(filename);
+            Instances data = source.getDataSet();
+            // setting class attribute if the data format does not provide this information
+            // For example, the XRFF format saves the class attribute information as well
+            if (data.classIndex() == -1)
+                data.setClassIndex(data.numAttributes() - 1);
+            
+            RandomBayes classifier = new RandomBayes();//Create the classifier (default config)
+            
+            Evaluation eval = new Evaluation(data);
+            eval.crossValidateModel(classifier, data, 10, new Random(1));
+            
+            System.out.println(eval.toSummaryString());
         } catch (Exception ex) {
+            ex.printStackTrace();
         }
     }
     
